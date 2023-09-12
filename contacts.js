49e38567ba78f0f84cb9d1a1b0592d73de1e69aa
luisQuintero32 <luispau2390@gmail.com>
2023-09-11 19:37:49 -0500

    commit 1

---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require("fs").promises;
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = "./db/contacts.json";
+
+function listContacts() {
+  return fs.readFile(contactsPath).then((data) => {
+    return JSON.parse(data.toString());
+  });
+}
+
+function getContactById(contactId) {
+  return listContacts().then((list) => {
+    const getById = list.find((contact) => contact.id === contactId);
+    return getById;
+  });
+}
+
+function removeContact(contactId) {
+  return listContacts().then((list) => {
+    const filteredList = list.filter((contact) => contact.id !== contactId);
+    return fs
+      .writeFile(contactsPath, JSON.stringify(filteredList), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+      .then(() => `Contact with id ${contactId} was successfully removed.`);
+  });
+}
+
+function addContact(name, email, phone) {
+  return listContacts().then((list) => {
+    const addUser = { id: uuidv4(), name, email, phone };
+    list.push(addUser);
+    return fs
+      .writeFile(contactsPath, JSON.stringify(list), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+      .then(() => `Contact was successfully created.`);
+  });
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,45 @@
+const contactsModule = require("./contacts.js");
+
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: refactorizar
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contactsModule.listContacts().then((list) => console.table(list));
+      break;
+
+    case "get":
+      contactsModule.getContactById(id).then((data) => console.table(data));
+      break;
+
+    case "add":
+      contactsModule.addContact(name, email, phone).then((msg) => {
+        console.log(msg);
+      });
+      break;
+
+    case "remove":
+      contactsModule.removeContact(id).then((msg) => {
+        console.log(msg);
+      });
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
